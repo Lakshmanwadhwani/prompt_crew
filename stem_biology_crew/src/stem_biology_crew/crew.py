819014da36698e285b69406ed21340d5e0ba99a2
lakshman Wadhwani <lmarkal.wadhwani@gmail.com>
2025-01-08 22:01:36 -0600

    2nd commit

--- stem_biology_crew/src/stem_biology_crew/crew.py
+++ stem_biology_crew/src/stem_biology_crew/crew.py
@@ -1,62 +1,63 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
-
-# If you want to run a snippet of code before or after the crew starts, 
-# you can use the @before_kickoff and @after_kickoff decorators
-# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+from crewai_tools import OpenAIAgentTool  # Import OpenAI tool if needed for tasks
 
 @CrewBase
-class StemBiologyCrew():
-	"""StemBiologyCrew crew"""
+class BiologyPromptCrew():
+    """BiologyPrompt crew for generating and validating biology-related prompts."""
 
-	# Learn more about YAML configuration files here:
-	# Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-	# Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
-	agents_config = 'config/agents.yaml'
-	tasks_config = 'config/tasks.yaml'
+    @agent
+    def navigator(self) -> Agent:
+        """Skill Identification Agent"""
+        return Agent(
+            config=self.agents_config['navigator'],  # Load Navigator config from YAML
+            verbose=True
+        )
 
-	# If you would like to add tools to your agents, you can learn more about it here:
-	# https://docs.crewai.com/concepts/agents#agent-tools
-	@agent
-	def researcher(self) -> Agent:
-		return Agent(
-			config=self.agents_config['researcher'],
-			verbose=True
-		)
+    @agent
+    def creator(self) -> Agent:
+        """Prompt Generation Agent"""
+        return Agent(
+            config=self.agents_config['creator'],  # Load Creator config from YAML
+            verbose=True,
+            tools=[OpenAIAgentTool()]  # Tool for GPT integration
+        )
 
-	@agent
-	def reporting_analyst(self) -> Agent:
-		return Agent(
-			config=self.agents_config['reporting_analyst'],
-			verbose=True
-		)
+    @agent
+    def inspector(self) -> Agent:
+        """Rubric Validation Agent"""
+        return Agent(
+            config=self.agents_config['inspector'],  # Load Inspector config from YAML
+            verbose=True
+        )
 
-	# To learn more about structured task outputs, 
-	# task dependencies, and task callbacks, check out the documentation:
-	# https://docs.crewai.com/concepts/tasks#overview-of-a-task
-	@task
-	def research_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['research_task'],
-		)
+    @task
+    def navigator_task(self) -> Task:
+        """Task for skill identification."""
+        return Task(
+            config=self.tasks_config['navigator_task'],  # Load task config from YAML
+        )
 
-	@task
-	def reporting_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['reporting_task'],
-			output_file='report.md'
-		)
+    @task
+    def creator_task(self) -> Task:
+        """Task for prompt generation."""
+        return Task(
+            config=self.tasks_config['creator_task'],  # Load task config from YAML
+        )
 
-	@crew
-	def crew(self) -> Crew:
-		"""Creates the StemBiologyCrew crew"""
-		# To learn how to add knowledge sources to your crew, check out the documentation:
-		# https://docs.crewai.com/concepts/knowledge#what-is-knowledge
+    @task
+    def inspector_task(self) -> Task:
+        """Task for rubric validation."""
+        return Task(
+            config=self.tasks_config['inspector_task'],  # Load task config from YAML
+        )
 
-		return Crew(
-			agents=self.agents, # Automatically created by the @agent decorator
-			tasks=self.tasks, # Automatically created by the @task decorator
-			process=Process.sequential,
-			verbose=True,
-			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
-		)
+    @crew
+    def crew(self) -> Crew:
+        """Creates the BiologyPrompt crew."""
+        return Crew(
+            agents=self.agents,  # Automatically created by the @agent decorator
+            tasks=self.tasks,  # Automatically created by the @task decorator
+            process=Process.sequential,  # Run tasks sequentially
+            verbose=True,
+        )
